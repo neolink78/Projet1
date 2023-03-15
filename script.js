d0bed310bd60b4e048561974009eb1c2fac48d21
Thibaut Resse <thibaut.resse@outlook.fr>
2023-03-15 16:15:43 +0100

    script

---
+++ script.js
@@ -0,0 +1,40 @@
+
+function dependingQuery (nber) {
+    switch(nber) {
+        case '1':
+            console.log("T'es nul");
+        break;
+        case '2':
+            console.log("T'es nul");
+        break;
+        case '3':
+            console.log("T'es nul");
+        break;
+        case '4':
+            console.log("T'es nul");
+        break;
+        case '5':
+            console.log("T'es nul");
+        break;
+        case '6':
+            console.log("T'es nul");
+        break;
+        case '7':
+            console.log("T'es nul");
+        break;
+        case '8':
+            console.log("T'es nul");
+        break;
+        case '9':
+            console.log("T'es nul");
+        break;
+        case '10':
+            console.log("T'es nul");
+        break;
+        default:
+            console.log("aucune question enregistrée")
+
+    }
+}
+
+const result = document.querySelector('.result');
